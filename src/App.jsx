16602043df5f76f260044d35b4f04681202f5b2c
ismail <ismaildimah@gmail.com>
2026-07-13 15:03:55 +0200

    Ajout des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { Cart } from "./pages/Cart";
+import { Login } from "./pages/Login";
+import { Product } from "./pages/Product";
+import { Register } from "./pages/Register";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
-
-  return <>
-  
-  </>
-
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/product/:id' element={<Product/>} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
